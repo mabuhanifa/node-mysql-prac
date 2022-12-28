c8a259855a258c9f6beeade3af0e16b9fb2346c0
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-29 03:07:51 +0600

    my sql db

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import mysql from "mysql";
 const app = express();
 const port = 5000;
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+
 app.get("/", (req, res) => {
   res.send("hello world from here");
 });
