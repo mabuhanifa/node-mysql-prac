3e83cdc98a608c29eb8e6f72d49ba3a2f93f9ee2
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-29 03:29:08 +0600

    data fetched

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+import cors from "cors";
 import express from "express";
 import mysql from "mysql";
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 5000;
+
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "root123",
   database: "test",
 });
 
 app.get("/", (req, res) => {
   res.send("hello world from here");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      res.json(err);
+    }
+    return res.json(data);
+  });
 });
 
 app.listen(port, () => {
